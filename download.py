86a81f1e60a9de8a0a7e18a5ee48b348335c94f0
Masahiro Morita <42367223+choroama@users.noreply.github.com>
2019-01-07 13:54:38 +0900

    Update download.py

--- download.py
+++ download.py
@@ -4,5 +4,4 @@ import subprocess
 df = pd.read_csv('kwfc-lat_assoc.csv')
 dg = df['KWFC_data_number'].astype(str)
 for i in dg:
-    subprocess.call(['curl', '-O', 'http://www.ioa.s.u-tokyo.ac.jp/~tmorokuma/tmp/clean_0' + i + '_3.fits'])
-#/Users/mmorita/kwfc-analysis/analysis/fitsfiles/'
+    subprocess.call(['curl', '-O', '[URL]' + i + '_3.fits'])
